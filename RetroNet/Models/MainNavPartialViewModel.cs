43a62a1b86ef602b76d11f0c0f3bec4808f6f898
Seth Holden Richard <37456216+sethdelrey@users.noreply.github.com>
2021-07-20 22:16:35 -0500

    - moved the nav to a partial view and added a dropdown to the nav to access Account/Manage

---
+++ RetroNet/Models/MainNavPartialViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _90sTest.Models
+{
+    public class MainNavPartialViewModel
+    {
+        public string Username { get; set; }
+    }
+}
