e906a2e8b4b1e979176d9daaa3c6249a5f49b815
Seth Holden Richard <holdenbrown10@gmail.com>
2021-09-28 22:16:29 -0500

    - fixing up blocks conversion and deploying to prod

---
+++ RetroNet/Controllers/BlocksController.cs
@@ -0,0 +1,47 @@
+﻿using _90sTest.Areas.Identity.Data;
+using _90sTest.Data;
+using _90sTest.Data.Extension;
+using _90sTest.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _90sTest.Controllers
+{
+    [Authorize]
+    public class BlocksController : Controller
+    {
+        private readonly UserManager<RetroNetUser> _userManager;
+        private readonly RetroNetContext _context;
+
+        public BlocksController(UserManager<RetroNetUser> userManager, RetroNetContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Index(string username)
+        {
+            var currentUserId = User.GetLoggedInUserId<string>();
+            var blockedUserId = _userManager.FindByNameAsync(username).Result.Id;
+            var user = _userManager.FindByNameAsync(username).Result;
+
+            var data = new BlocksModel()
+            { 
+                BlockedByList = _context.Blocks.AsNoTracking().Where(f => f.UserId.Equals(user.Id)).Include(f => f.Blocker).Select(f => f.Blocker).ToList(),
+                BlockedList = _context.Blocks.AsNoTracking().Where(f => f.BlockerId.Equals(user.Id)).Include(f => f.User).Select(f => f.User).ToList(),
+                User = user,
+                IsBlocked = _context.Blocks.AsNoTracking().Where(f => f.UserId.Equals(user.Id) && f.BlockerId.Equals(currentUserId)).ToList().Count != 0,
+                BlockeByCount = _context.Blocks.AsNoTracking().Where(f => f.UserId.Equals(blockedUserId)).Count(),
+                BlockedCount = _context.Blocks.AsNoTracking().Where(f => f.BlockerId.Equals(blockedUserId)).Count()
+            };
+
+            return View("Blocks", data);
+        }
+    }
+}
---
+++ RetroNet/Models/BlocksModel.cs
@@ -0,0 +1,24 @@
+﻿using _90sTest.Areas.Identity.Data;
+using _90sTest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _90sTest.Models
+{
+    public class BlocksModel
+    {
+        public List<RetroNetUser> BlockedByList { get; set; }
+
+        public List<RetroNetUser> BlockedList { get; set; }
+
+        public RetroNetUser User { get; set; }
+
+        public bool IsBlocked { get; set; }
+
+        public int BlockeByCount { get; set; }
+
+        public int BlockedCount { get; set; }
+    }
+}
